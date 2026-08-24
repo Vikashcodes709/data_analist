005f0525c8f3b2a354baa82619f5dd83f1bc32c9
Vikashcodes709 <singhvikash77898@gmail.com>
2026-08-24 12:19:13 +0530

    data

---
+++ c1/24_Aug_2026/dictionary.py
@@ -0,0 +1,15 @@
+'''a={101:'Aman',105:'sheat',110:'zomato',120:'uncle lays'}
+print(a)'''
+
+'''na={}
+e=int(input("Give the number of student"))
+i=1
+while i<=e:
+    name=input("Student Name")
+    marks=input("marks of student")
+    na[name]=marks
+    i=i+1
+print("Name of student","\t","Marks")
+for x in na:
+    print("\t",x,"\t\t",na[x])'''
+    
---
+++ c1/24_Aug_2026/panda's.py
@@ -0,0 +1,8 @@
+## pandas 
+efficient data handling
+data cleaning & preprocessing 
+flexible O/P and I/P (input and output)
+integation seamless
+import pandas as pd
+series=
+a=pd
