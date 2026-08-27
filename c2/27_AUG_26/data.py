864d6c2357fdfb2a249271fa948b43bbf5c755f9
Vikashcodes709 <singhvikash77898@gmail.com>
2026-08-27 12:32:35 +0530

    data

--- c2/27_AUG_26/data.py
+++ c2/27_AUG_26/data.py
@@ -1,14 +1,18 @@
-import pandas as pd
+import pandas as pd; print(pd.__file__); print(pd.__version__)
+
+
 
 data = {
     'Name': ['Mahesh', 'Paani', 'Ramesh'],
     'Age': [25, 30, 35],
     'City': ['Mumbai', 'Banaras', 'Delhi'],
-    'pincode': [221101, 221005, 221102],
+    'Pincode': [221101, 221005, 221102],
     'Department': ['IT', 'CSE', 'Accounts'],
     'Skills': ['Python', 'Java', 'Data Analysis']
 }
 
 dept = pd.DataFrame(data)
+print(pd.__file__)
 
-print(dept)
+#print(dept)
+print(dept.tail(2))
