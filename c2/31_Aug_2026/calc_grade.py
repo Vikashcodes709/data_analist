ef189408d678fa372a6e09d5896d85dd12df62c1
Vikashcodes709 <singhvikash77898@gmail.com>
2026-08-31 12:01:54 +0530

    deta

---
+++ c2/31_Aug_2026/calc_grade.py
@@ -0,0 +1,40 @@
+'''def find_avg_marks(marks):
+    sum_of_marks=sum(marks)
+    no_of_subject=len(marks)
+    avg_marks=sum_of_marks/no_of_subject
+    return avg_marks 
+
+def calc_grade(avg_marks):
+    if avg_marks >=80:
+        grade='A'
+    elif avg_marks >=65:
+        grade='B'
+    elif avg_marks >=55:
+        grade='C'
+    else:
+        grade='F'
+    return grade
+avg_marks=find_avg_marks(marks)
+grade=calc_grade(avg_marks)
+print(grade)'''
+
+#TABLE PRINT***
+'''n=5
+i=1
+while i<=10:
+    print(i*1)
+    i=i+1
+
+n1=6
+j=1
+while j<=10:
+    print(n1*j)
+    j=i+1'''
+
+def table(n):
+    i=1
+    while i<=10:
+        print(n*1)
+        i=i+1
+    
+        
---
+++ c2/31_Aug_2026/function.py
@@ -0,0 +1,10 @@
+def find_avg_marks(marks):
+    sum_of_marks=sum(marks)
+    no_of_subject=len(marks)
+    avg_marks=sum_of_marks/no_of_subject
+    return avg_marks    #positional argument
+
+marks=[40,45,150,118,25,85]
+avg_marks=find_avg_marks(marks)
+print("Avg marks:",avg_marks)
+    
