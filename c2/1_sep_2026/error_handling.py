2d63138f218e485b3840b9a557b77ebb160f1d90
Vikashcodes709 <singhvikash77898@gmail.com>
2026-09-01 12:20:01 +0530

    new

---
+++ c2/1_sep_2026/error_handling.py
@@ -0,0 +1,58 @@
+'''try:
+    n=int(input("Number 1:"))
+    m=int(input("Number 2:"))
+    res=n/m
+except:
+    print("M cannot be zero (0), kindly try anain with different number")
+print("The solution is done, Hence closing the program")'''
+
+
+
+'''try:
+    n=int(input("Number 1:"))
+    m=int(input("Number 2:"))
+    res=n/m
+    print("result is:",res)
+except ZeroDivisionError:
+    print("M cannot be zero (0), kindly try anain with different number")
+print("The program is done, Hence closing it.")'''
+
+
+
+'''try:
+    n=int(input("Number 1:"))
+    m=int(input("Number 2:"))
+    res=n/m
+    print("result is:",res)
+
+    #indexing exception
+    my_list=[1,2,3] #3 variables we have taken or declair
+    a=int(input("Enter the index position"))
+    print(my_list[a])
+except ZeroDivisionError:
+    print("M cannot be zero (0), kindly try anain with different number")
+print("The program is done, Hence closing it.")'''
+
+
+
+'''try:
+    n=int(input("Number 1:"))
+    m=int(input("Number 2:"))
+    res=n/m
+    print("result is:",res)
+
+    #indexing exception
+    my_list=[1,2,3] #3 variables we have taken or declair
+    a=int(input("Enter the index position"))
+    print(1/0)   
+except ZeroDivisionError:
+    print("M cannot be zero (0), kindly try anain with different number")
+print("The program is done, Hence closing it.")'''
+
+
+
+
+
+
+
+
---
+++ c2/1_sep_2026/msg_txt.py
@@ -0,0 +1,11 @@
+'''f=open("msg.txt","r")
+content=f.read(20)
+print(content)
+f.close()'''
+
+'''with open("python.txt","w") as f:
+    f.write("this is day 1 of sept 2026.")
+    f.write("today i am reading error handling")'''
+    
+
+
